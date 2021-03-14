f61d893e335af0c467436e53671ac1e6f3d17f46
Johnson Ogwuru <ogwurujohnson@gmail.com>
2021-03-14 23:25:37 +0100

    start work on bridge base

--- contracts/BridgeBase.sol
+++ contracts/BridgeBase.sol
@@ -1,2 +1,24 @@
 pragma solidity ^0.8.0;
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './IToken.sol';
 
+contracts BridgeBase {
+  address public admin;
+  IToken public token;
+  uint public nonce;
+  mapping(uint => bool) public processedNonces;
+
+  enum Step { Burn, Mint }
+  event Transfer {
+    address from,
+    address to,
+    uint amount,
+    uint date,
+    uint nonce,
+    Step indexed step
+  };
+
+  constructor(address _token) {
+    admin = msg.sender;
+  }
+}
