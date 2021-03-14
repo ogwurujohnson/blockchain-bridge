72b680650343629c07e43e8d51fcb08dcaada800
Johnson Ogwuru <ogwurujohnson@gmail.com>
2021-03-15 00:04:50 +0100

    create token base contract

--- contracts/TokenBase.sol
+++ contracts/TokenBase.sol
@@ -13,4 +13,19 @@ contract TokenBase is ERC20 {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     admin = msg.sender;
   }
+
+  function updateAdmin(address newAdmin) external {
+    require(msg.sender == admin, 'only admin');
+    admin = newAdmin;
+  }
+
+  function mint(address to, uint amount) external {
+    require(msg.sender == admin, 'only admin');
+    _mint(to, amount);
+  }
+
+  function burn(address owner, uint amount) external {
+    require(msg.sender == admin, 'only admin');
+    _burn(owner, amount);
+  }
 }
